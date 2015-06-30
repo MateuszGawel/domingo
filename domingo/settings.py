2648dbcaa040042e91e71c4a78f2bb6b26285988
MateuszGawel <mg.mateuszgawel@gmail.com>
2015-06-30 16:00:14 +0200

    próba deploya 3

--- domingo/settings.py
+++ domingo/settings.py
@@ -97,8 +97,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATIC_ROOT = '/opt/myenv/static/'
+STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
