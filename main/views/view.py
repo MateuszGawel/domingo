22e95e46d32c6b2f57dff322e8dbb7c5ef20ac79
mantkowicz <antek15@gmail.com>
2015-08-10 16:11:05 +0200

    mail

--- main/views/view.py
+++ main/views/view.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.core.urlresolvers import reverse
+from django.core.mail import send_mail
 
 
 def index(request):
@@ -22,8 +24,12 @@ def __do_login(request):
         else:
             return render(request, 'main/login.html', {'error_message': "Sorry, the account is not active."})
     else:
-        return render(request, 'main/login.html', {'error_message': "Please provide correct credentials"})
+        return render(request, 'main/login.html', {'error_message': "Incorrect credentials (<a href='"+reverse("main:reset_password")+"'>reset password</a>)"})
 
 def do_logout(request):
     logout(request)
     return redirect("main:index")
+
+def reset_password(request):
+    send_mail('Subject here', 'Here is the message.', 'from@example.com', ['michal.antkowicz@comarch.pl'], fail_silently=False)
+    return render(request, 'main/login.html', {'error_message': "The new password has been sent to you!"})
